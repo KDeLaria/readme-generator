6f2ea9f7dfca660a934567759bfe967badfe5c86
KDeLaria <149177749+KDeLaria@users.noreply.github.com>
2024-01-18 15:08:31 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = ["What is the title of your for your project?", 
+"What is a description of your project?", "What are the installation instructions for your project?",
+"How do you use this project?", "What are the contribution guidlines for the project?", 
+"What tests need to be done on this project?"];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {
+    
+}
+
+// Function call to initialize app
+init();
